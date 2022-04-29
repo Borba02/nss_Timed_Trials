afa1d71d05e76333020a56b9325075bd8bbc7b20
Bryce Borba <borbaesb@gmail.com>
2022-04-28 18:56:02 -0700

    Added fullstack Website view/functionality

---
+++ TimedTrials/Controllers/WebsiteController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using TimedTrials.Repositories;
+using TimedTrials.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TimedTrials.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebsiteController : ControllerBase
+    {
+        private readonly IWebsiteRepository _websiteRepository;
+
+        public WebsiteController(
+            IWebsiteRepository websiteRepository)
+        {
+            _websiteRepository = websiteRepository;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_websiteRepository.GetAll());
+        }
+    }
+}
---
+++ TimedTrials/Repositories/WebsiteRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using TimedTrials.Models;
+using Microsoft.Data.SqlClient;
+using TimedTrials.Utils;
+
+namespace TimedTrials.Repositories
+{
+
+    public class WebsiteRepository : BaseRepository, IWebsiteRepository
+    {
+        public WebsiteRepository(IConfiguration configuration) : base(configuration) { }
+        public List<Website> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, [Name], Url AS WebsiteUrl FROM Website";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var websites = new List<Website>();
+                        while (reader.Read())
+                        {
+                            websites.Add(new Website()
+                            {
+                                Id = DbUtils.GetInt(reader, "Id"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Url = DbUtils.GetString(reader, "WebsiteUrl")
+
+
+                            });
+                        }
+                        return websites;
+
+                    }
+                }
+            }
+        }
+    }
+
+}
